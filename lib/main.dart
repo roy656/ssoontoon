552c9268d7c6490939883b95630640f78bead5b0
roy656 <v0o0605@gmail.com>
2023-03-08 23:56:27 +0900

    additional Texts, Containers are made

--- lib/main.dart
+++ lib/main.dart
@@ -79,7 +79,63 @@ class App extends StatelessWidget {
                         bgColor: Color(0xFF1F2123),
                         textColor: Colors.white)
                   ],
-                )
+                ),
+                const SizedBox(
+                  height: 100,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Wallets',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 36,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      'View All',
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.8),
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                    decoration: const BoxDecoration(
+                      color: Color(0xFF1F2123),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 20, horizontal: 10),
+                      child: Row(
+                        children: [
+                          Column(
+                            children: [
+                              const Text(
+                                'Euro',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              const SizedBox(
+                                height: 15,
+                              ),
+                              Row(
+                                children: const [
+                                  Text('6 428'),
+                                  Text('EUR'),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    )),
               ],
             ),
           )),
