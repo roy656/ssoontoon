f82e2e49c963969f344933d9fa8679d9c7ee43ad
roy656 <v0o0605@gmail.com>
2023-03-20 22:00:35 +0900

    Stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,142 +1,49 @@
 import 'package:flutter/material.dart';
-import 'package:ssoontoon/widgets/button.dart';
-import 'package:ssoontoon/widgets/card.dart';
 
-// 정적 화면
 void main() {
   runApp(App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+
+  void onClicked() {
+    setState(() {
+      counter += 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: const Color.fromARGB(255, 18, 18, 18),
-          body: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(
-                    height: 80,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          const Text(
-                            'Good to see you Roy',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 28,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          Text(
-                            'Welcome Back!',
-                            style: TextStyle(
-                              fontSize: 18,
-                              color: Colors.white.withOpacity(0.8),
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 60,
-                  ),
-                  Text(
-                    'Total Balance',
-                    style: TextStyle(
-                        color: Colors.white.withOpacity(0.8), fontSize: 25),
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  Text(
-                    '\$5 123 090',
-                    style: TextStyle(
-                        color: Colors.white.withOpacity(0.8),
-                        fontSize: 50,
-                        fontWeight: FontWeight.w700),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Button(
-                          // Button 역할을 하는 Container 클래스를 만들어서 사용
-                          text: 'Transger',
-                          bgColor: Color(0xfff1b33b),
-                          textColor: Colors.black),
-                      Button(
-                          text: 'Request',
-                          bgColor: Color(0xFF1F2123),
-                          textColor: Colors.white)
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 50,
-                  ),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
-                        'Wallets',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 36,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      Text(
-                        'View All',
-                        style: TextStyle(
-                          color: Colors.white.withOpacity(0.8),
-                          fontSize: 18,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  const CurrencyCard(
-                    name: 'Euro',
-                    code: 'EUR',
-                    amount: '6 428',
-                    icon: Icons.euro_rounded,
-                    isInverted: false,
-                    offsetOrder: 0,
-                  ),
-                  const CurrencyCard(
-                    name: 'Bitcoin',
-                    code: 'BTC',
-                    amount: '9 758',
-                    icon: Icons.currency_bitcoin,
-                    isInverted: true,
-                    offsetOrder: 1,
-                  ),
-                  const CurrencyCard(
-                    name: 'Dollar',
-                    code: 'USD',
-                    amount: '428',
-                    icon: Icons.attach_money_outlined,
-                    isInverted: false,
-                    offsetOrder: 2,
-                  ),
-                ],
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Click Count',
+                style: TextStyle(fontSize: 30),
               ),
-            ),
-          )),
+              Text(
+                '$counter',
+                style: const TextStyle(fontSize: 30),
+              ),
+              IconButton(
+                iconSize: 50,
+                onPressed: onClicked,
+                icon: const Icon(Icons.add_box_rounded),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/main1.dart
@@ -0,0 +1,142 @@
+// import 'package:flutter/material.dart';
+// import 'package:ssoontoon/widgets/button.dart';
+// import 'package:ssoontoon/widgets/card.dart';
+
+// // 정적 화면
+// void main() {
+//   runApp(App());
+// }
+
+// class App extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//           backgroundColor: const Color.fromARGB(255, 18, 18, 18),
+//           body: SingleChildScrollView(
+//             child: Padding(
+//               padding: const EdgeInsets.symmetric(horizontal: 20),
+//               child: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: [
+//                   const SizedBox(
+//                     height: 80,
+//                   ),
+//                   Row(
+//                     mainAxisAlignment: MainAxisAlignment.end,
+//                     children: [
+//                       Column(
+//                         crossAxisAlignment: CrossAxisAlignment.end,
+//                         children: [
+//                           const Text(
+//                             'Good to see you Roy',
+//                             style: TextStyle(
+//                               color: Colors.white,
+//                               fontSize: 28,
+//                               fontWeight: FontWeight.w600,
+//                             ),
+//                           ),
+//                           Text(
+//                             'Welcome Back!',
+//                             style: TextStyle(
+//                               fontSize: 18,
+//                               color: Colors.white.withOpacity(0.8),
+//                             ),
+//                           ),
+//                         ],
+//                       )
+//                     ],
+//                   ),
+//                   const SizedBox(
+//                     height: 60,
+//                   ),
+//                   Text(
+//                     'Total Balance',
+//                     style: TextStyle(
+//                         color: Colors.white.withOpacity(0.8), fontSize: 25),
+//                   ),
+//                   const SizedBox(
+//                     height: 8,
+//                   ),
+//                   Text(
+//                     '\$5 123 090',
+//                     style: TextStyle(
+//                         color: Colors.white.withOpacity(0.8),
+//                         fontSize: 50,
+//                         fontWeight: FontWeight.w700),
+//                   ),
+//                   const SizedBox(
+//                     height: 20,
+//                   ),
+//                   Row(
+//                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                     children: const [
+//                       Button(
+//                           // Button 역할을 하는 Container 클래스를 만들어서 사용
+//                           text: 'Transger',
+//                           bgColor: Color(0xfff1b33b),
+//                           textColor: Colors.black),
+//                       Button(
+//                           text: 'Request',
+//                           bgColor: Color(0xFF1F2123),
+//                           textColor: Colors.white)
+//                     ],
+//                   ),
+//                   const SizedBox(
+//                     height: 50,
+//                   ),
+//                   Row(
+//                     crossAxisAlignment: CrossAxisAlignment.end,
+//                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                     children: [
+//                       const Text(
+//                         'Wallets',
+//                         style: TextStyle(
+//                           color: Colors.white,
+//                           fontSize: 36,
+//                           fontWeight: FontWeight.w600,
+//                         ),
+//                       ),
+//                       Text(
+//                         'View All',
+//                         style: TextStyle(
+//                           color: Colors.white.withOpacity(0.8),
+//                           fontSize: 18,
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                   const SizedBox(
+//                     height: 20,
+//                   ),
+//                   const CurrencyCard(
+//                     name: 'Euro',
+//                     code: 'EUR',
+//                     amount: '6 428',
+//                     icon: Icons.euro_rounded,
+//                     isInverted: false,
+//                     offsetOrder: 0,
+//                   ),
+//                   const CurrencyCard(
+//                     name: 'Bitcoin',
+//                     code: 'BTC',
+//                     amount: '9 758',
+//                     icon: Icons.currency_bitcoin,
+//                     isInverted: true,
+//                     offsetOrder: 1,
+//                   ),
+//                   const CurrencyCard(
+//                     name: 'Dollar',
+//                     code: 'USD',
+//                     amount: '428',
+//                     icon: Icons.attach_money_outlined,
+//                     isInverted: false,
+//                     offsetOrder: 2,
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           )),
+//     );
+//   }
+// }
